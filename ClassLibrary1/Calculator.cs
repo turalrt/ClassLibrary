e6294d46aa7f6364cb77105a1b0d0b2d0343bcc6
Tural Taghiyev <turalrt@code.edu.az>
2021-12-14 10:42:50 +0400

    first commit

---
+++ ClassLibrary1/Calculator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+    public class Calculator
+    {
+        public int n;
+        public int m;
+        public void Add(int n, int m)
+        {
+            Console.WriteLine(n + m);
+        }
+        public void Sub(int n, int m)
+        {
+            Console.WriteLine(n - m);
+        }
+        public void Mul(int n, int m)
+        {
+            Console.WriteLine(n * m);
+        }
+        public void Div(int n, int m)
+        {
+            Console.WriteLine(n / m);
+        }
+    }
+}
---
+++ ConsoleApp28/Program.cs
@@ -0,0 +1,17 @@
+﻿using ClassLibrary1;
+using System;
+
+namespace ConsoleApp28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calculator = new Calculator();
+            calculator.Add(15, 5);
+            calculator.Sub(15, 5);
+            calculator.Mul(15, 5);
+            calculator.Div(15, 5);
+        }
+    }
+}
